ebe0e004259df62439b0f97c801407a5eee41da7
Kiara Lee <kiara0515@gmail.com>
2025-03-13 16:46:31 -0400

    Created Variables for input fields

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,1 +1,23 @@
-console.log("Hello WOrld");
+const loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", logUserin);
+
+async function logUserin(event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    const loginData = {
+        "email": email.value,
+        "password": password.value
+    };
+
+    try {
+        const response = await fetch("http://localhost:5678/users/login", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(loginData)
+        })
