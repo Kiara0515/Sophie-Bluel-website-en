09bd74cbbc2f6ff4632a58c00f09f7eea0c17416
Kiara Lee <kiara0515@gmail.com>
2025-03-14 15:57:53 -0400

    Variables updated

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -4,13 +4,15 @@ loginForm.addEventListener("submit", logUserin);
 async function logUserin(event) {
     event.preventDefault();
 
-    const email = document.getElementById("email");
-    const password = document.getElementById("password");
+    
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
+    
     const loginData = {
-        "email": email.value,
-        "password": password.value
-}};
+        email: email,
+        password: password
+    };
 
     try {
         const response = await fetch("http://localhost:5678/users/login", {
@@ -20,4 +22,16 @@ async function logUserin(event) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(loginData)
-    }}
+        });
+
+        if (!response.ok) {
+            alert("Incorrect email or password");
+        } else {
+            try {
+                const data = await response.json();
+                console.log("Data: " + JSON.stringify(data));
+            } catch (error) {
+                console.error("Error during login:", error);
+                alert("An error occurred. Please try again.");
+            }
+        }
