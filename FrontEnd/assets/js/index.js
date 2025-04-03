86e5845864e9894064da310c0d5b601aeb729fef
Kiara Lee <kiara0515@gmail.com>
2025-04-03 15:47:13 -0400

    Added functions for Categories

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -39,21 +39,20 @@ displayWorksData();
 //Show and hide button and banner in HTML and CSS//
 //Show and hide both if token is in session storage//
 
-
 // edit and login logout variables
 const logoutLink = document.getElementById("logout");
 const loginLink = document.getElementById("login");
 const editBanner = document.querySelector(".edit-banner");
 const editButton = document.querySelector(".edit-button");
 
-
 function checkLoginStatus() {
 	// will return true or false
 	return sessionStorage.getItem("token") !== null;
 }
-
+// is logged in true or false
 const isLoggedIn = checkLoginStatus();
 
+//checking if links are there showing and hiding login/logout buttons
 if (loginLink && logoutLink) {
 	if (isLoggedIn) {
 		loginLink.style.display = "none";
@@ -67,9 +66,9 @@ if (loginLink && logoutLink) {
 		editButton.style.display = "none";
 	}
 }
-
+//listening for a click then logging user out
 logoutLink.addEventListener("click", logoutUser);
-
+//logging user out
 function logoutUser() {
 	alert("logging out!");
 	// ✅ clear sessionStorage.clear();
@@ -77,3 +76,34 @@ function logoutUser() {
 	// ✅ send user to login.html
 	window.location.href = "login.html";
 }
+
+async function fetchCatergories() {
+	try {
+		const reponse = await fetch("http://localhost:5678/api/categories");
+		if (!reponse.ok) {
+			alert("Categories is not ok!");
+		} else {
+			const categoriesData = await response.json();
+			console.log("categoriesData" + JSON.stringify(data));
+
+			return categoriesData;
+		}
+	} catch (error) {
+		console.error("Error,error");
+		alert("An error occurred. Please try again.");
+	}
+}
+
+async function displayCatergories() {
+	const categories = await fetchCatergories(
+		"http://localhost:5678/api/categories"
+	);
+}
+
+async function init() {
+	const works = await fetchWorksData();
+	displayWorksData(works);
+	displayCategories();
+}
+
+init();
